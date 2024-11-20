574075d94aff511a3700e69ba7169f52c6e60160
Gabriel Teixeira <gabrielm.teixeira@ua.pt>
2024-11-20 11:58:31 +0000

    autocomplit barra de pesquisa

--- coffee_shop/src/App.jsx
+++ coffee_shop/src/App.jsx
@@ -5,13 +5,47 @@ import { CiShoppingCart } from "react-icons/ci";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { RxCross1 } from "react-icons/rx";
 import { useNavigate } from 'react-router-dom';
-import  assets  from './assets/imgs/assets';
+import assets from './assets/imgs/assets';
 
 function App() {
 
   const [count, setCount] = useState(0)
-  const [nav , setNav] = useState(false)
+  const [nav, setNav] = useState(false)
   const navigate = useNavigate();
+
+  const [searchInput, setSearchInput] = useState("");
+  const [suggestions, setSuggestions] = useState([]);
+  const items = [
+    "Argentino",
+    "Caramelo",
+    "Cappuccino",
+    "Preto",
+    "Americano",
+    "Mocha",
+    "Macchiato",
+    "Flat White",
+    "Cold Brew",
+    "Baunilha",
+  ];
+
+  const handleSearchChange = (e) => {
+    const input = e.target.value;
+    setSearchInput(input);
+
+    if (input.length > 0) {
+      const filteredSuggestions = items.filter((item) =>
+        item.toLowerCase().includes(input.toLowerCase())
+      );
+      setSuggestions(filteredSuggestions);
+    } else {
+      setSuggestions([]);
+    }
+  };
+
+  const handleSuggestionClick = (suggestion) => {
+    setSearchInput(suggestion);
+    setSuggestions([]);
+  };
 
   const handleOrderNow = () => {
     navigate('/Coffe');
@@ -23,12 +57,21 @@ function App() {
         <div className='absolute top-0 left-0 w-full h-screen bg-black/50 text-white'>
           <nav className='nav_style text-white lg:flex items-center p-5 justify-between'>
             <div className='flex items-center justify-between'>
-            <div className='flex border-1 gap-2 p-1  items-center bg-clip-padding backdrop-filter backdrop-blur-xl bg-opacity-60 rounded-md'>
-              <CiSearch size={35}/>
-              <input type='text' placeholder='Search' className='outline-none bg-transparent'></input>
-            </div>
-            <div className='cursor-pointer lg:hidden'>
-              <GiHamburgerMenu onClick={()=>{setNav(!nav)}} size={35}/>
+              <div className='flex border-1 gap-2 p-1 items-center bg-clip-padding backdrop-filter backdrop-blur-xl bg-opacity-60 rounded-md relative'>
+                <CiSearch size={35} />
+                <input type='text' placeholder='Search' className='outline-none bg-transparent' value={searchInput} onChange={handleSearchChange}
+                />
+                {suggestions.length > 0 && (
+                  <ul className='absolute top-[3rem] left-0 w-full bg-white text-black rounded-md shadow-lg z-10'>
+                    {suggestions.map((suggestion, index) => (
+                      <li key={index} className='p-2 hover:bg-gray-200 cursor-pointer' onClick={() => handleSuggestionClick(suggestion)}> {suggestion}
+                      </li>
+                    ))}
+                  </ul>
+                )}
+              </div>
+              <div className='cursor-pointer lg:hidden'>
+                <GiHamburgerMenu onClick={() => { setNav(!nav) }} size={35} />
               </div>
             </div>
             <ul className='lg:flex gap-20 text-[0.9rem] hidden'>
@@ -38,11 +81,11 @@ function App() {
               <li className='cursor-pointer'>DELIVERY</li>
             </ul>
             <div className='cursor-pointer mr-[1rem] hidden lg:block'>
-              <CiShoppingCart  size={35}/></div>
+              <CiShoppingCart size={35} /></div>
           </nav>
           <div>
-          <ul className={nav?'lg:flex-row flex flex-col gap-5 mx-auto text-center lg:gap-20 p-[2.3rem] text-[3rem] lg:text-[0.9rem] absolute bg-black top-0 left-0 h-screen w-full md:w-[70%] duration-1000 ':'duration-1000 top-[-100%] hidden'}>
-              <li className='ml-[90%] top-[-1rem cursor-poiter]'><RxCross1 onClick={()=>{setNav(!nav)}}/></li>
+            <ul className={nav ? 'lg:flex-row flex flex-col gap-5 mx-auto text-center lg:gap-20 p-[2.3rem] text-[3rem] lg:text-[0.9rem] absolute bg-black top-0 left-0 h-screen w-full md:w-[70%] duration-1000 ' : 'duration-1000 top-[-100%] hidden'}>
+              <li className='ml-[90%] top-[-1rem cursor-poiter]'><RxCross1 onClick={() => { setNav(!nav) }} /></li>
               <li className='cursor-pointer'>HOME</li>
               <li className='cursor-pointer'>ABOUT US</li>
               <li className='cursor-pointer'>MENU</li>
@@ -52,9 +95,9 @@ function App() {
           <div className='mt-[6%] lg:mt-[4%] w-full lg:w-[50%] mx-auto'>
             <div><p className='subtitle text-center lg:text-[1.5rem] lg:text-left font-bold'>Where every sip tell a story...</p></div>
             <div><h1 className='title text-[5rem] lg:text-[8rem] font-bold mx-auto text-center lg:text-left'>Brew & Blend Coffe</h1></div>
-            <div className='mx-auto lg:mx-0 text-center text-[1.3rem] bg-yellow-800 w-[80%] md:w-[70%] lg:w-[50%] font-bold px-2 py-3 border-2 border-yellow-800 rounded-[1.5rem]'> 
+            <div className='mx-auto lg:mx-0 text-center text-[1.3rem] bg-yellow-800 w-[80%] md:w-[70%] lg:w-[50%] font-bold px-2 py-3 border-2 border-yellow-800 rounded-[1.5rem]'>
               <button onClick={handleOrderNow}> Order Now</button>
-          </div>
+            </div>
           </div>
         </div>
         <img className='w-full top-0 left-0 h-screen object-cover ' src={assets.coffe1} alt="Coffee" />
